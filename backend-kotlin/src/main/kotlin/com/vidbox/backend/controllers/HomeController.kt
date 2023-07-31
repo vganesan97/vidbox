dc554e33c55271bf979e93518bc4c3193f2b1395
Vishaal Ganesan <vishaal.kganesan@gmail.com>
2023-07-31 05:08:33 -0400

    update base page for backend api

--- backend-kotlin/src/main/kotlin/com/vidbox/backend/controllers/HomeController.kt
+++ backend-kotlin/src/main/kotlin/com/vidbox/backend/controllers/HomeController.kt
@@ -1,28 +1,20 @@
 package com.vidbox.backend.controllers
 
 import com.google.cloud.storage.*
-import com.vidbox.backend.repos.GroupInfoRepository
-import com.vidbox.backend.repos.GroupMemberRepository
-import com.vidbox.backend.repos.UserRepository
-import com.vidbox.backend.services.FirebaseService
-import com.vidbox.backend.services.GCSService
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 
 @RestController
-class HomeController(private val userRepository: UserRepository,
-                     private val groupInfoRepository: GroupInfoRepository,
-                     private val groupMemberRepository: GroupMemberRepository,
-                     private val firebaseService: FirebaseService,
-                     private val gcsService: GCSService) {
+class HomeController() {
 
     @GetMapping("/")
     fun home(): Any {
         return """
-            <h1> the backend service has started </h1>
-        """.trimIndent()
+        <h1 style="color: green;"> The backend service has started 😊 </h1>
+    """.trimIndent()
     }
+
 
     @GetMapping("/test")
     fun getAllMovies(): Any {
